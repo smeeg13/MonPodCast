b0fd85cc47b41f707c30f428ac4f08c593169c1e
smeg13 <87640794+smeeg13@users.noreply.github.com>
2023-03-06 09:19:25 +0100

    class object

---
+++ monpodcast-project/src/pages/models/podcast.js
@@ -0,0 +1,10 @@
+export class Podcast {
+
+
+    constructor(name, description,date, duration ){
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.duration = duration;
+    }
+}
