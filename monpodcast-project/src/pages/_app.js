ceff933301919aa446cbb220a83588d9237448b2
Pauli Hemmi <lolled.mr@gmail.com>
2023-03-08 23:21:26 +0200

    Added player.js with an example data

--- monpodcast-project/src/pages/_app.js
+++ monpodcast-project/src/pages/_app.js
@@ -1,10 +1,13 @@
 import Header from "../components/Header";
+import Player from "../components/player";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+
+      <Player audioSrc="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
     </>
   );
 }
